29058974ea7ead52e24465449b2d9cee9cff0cff
msuarz <mikemps@gmail.com>
2010-06-19 22:12:43 -0400

    refined first feature

--- features/step_definitions/jira_task_board_steps.rb
+++ features/step_definitions/jira_task_board_steps.rb
@@ -2,6 +2,6 @@ When /^akira reads jira task board$/ do
   @jira = Akira::Jira.new
 end
 
-Then /^it should contain the stories$/ do
+Then /^it should load the stories$/ do
   @jira.stories.should_not be_empty
 end
